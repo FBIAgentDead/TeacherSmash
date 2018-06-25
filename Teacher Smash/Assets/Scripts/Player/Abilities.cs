d4b5144a97a6de6fa081716d135251ca095bf704
DimitrivanManen <17382@ma-web.nl>
2018-06-25 19:42:21 +0200

    Update

---
+++ Teacher Smash/Assets/Scripts/Player/Abilities.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Abilities : MonoBehaviour {
+
+    private float ultCharge;
+    //private MovementManager playerMovement;
+    private Animator attackAnim;
+    private Rigidbody player;
+
+    private void Awake()
+    {
+        //playerMovement = GetComponent<MovementManager>();
+        player = GetComponent<Rigidbody>();
+        attackAnim = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+         
+        if (ultCharge < 100)
+            ultCharge += Time.deltaTime;
+        
+    }
+
+    public void NeutralLeft()
+    {
+    }
+    public void NeutralRight()
+    {
+    }
+    public void NeutralUp()
+    {
+    }
+    public void NeutralDown()
+    {
+    }
+    public void Neutral()
+    {
+    }
+    public void SpecialLeft()
+    {
+    }
+    public void SpecialRight()
+    {
+    }
+    public void SpecialUp()
+    {
+    }
+    public void SpecialDown()
+    {
+    }
+    public void Special()
+    {
+        if (ultCharge > 99)
+            Debug.Log("UsedUlt");
+    }
+
+}
